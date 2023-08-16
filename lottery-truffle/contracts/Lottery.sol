ac52e139eba347b4778e93d42aa3e5da3562dc88
KimJinkYU <jinkyu1996@hanmail.net>
2023-08-16 17:33:07 +0900

    Feat: pickWinner and onlyOwner modifier

--- lottery-truffle/contracts/Lottery.sol
+++ lottery-truffle/contracts/Lottery.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.15;
 contract Lottery {
     address public owner;
     address payable[] public players; // 입출금을 위한 payable
+    uint256 public lotteryId;
+    mapping(uint256 => address) public lotteryHistory;
 
     constructor() {
         owner = msg.sender;
@@ -28,6 +30,26 @@ contract Lottery {
     // 랜덤값 반환 - 블록 내부의 값을 이용한 랜덤값 생성, 안전하지 않음
     function getRandomNumberV1() public view returns (uint256) {
         return uint256(keccak256(abi.encodePacked(owner, block.timestamp)));
+    }
+
+    // 승자 선정
+    function pickWinner() public onlyOwner {
+        uint256 index = getRandomNumberV1() % players.length;
+
+        // reentrancy attack을 방지하기 위해 다른 contract 호출 전 상태 변경을 진행해야 한다.
+        lotteryHistory[lotteryId] = players[index];
+        lotteryId++;
+
+        (bool success, ) = players[index].call{value: address(this).balance}("");
+        require (success, "Failed to send Ether");
+
+        players = new address payable[](0);
+    }
+
+    // 승자 선정 함수 호출 조건 설정 - 코드 반복을 제거하기 위해 modifier 사용, require와 거의 동일
+    modifier onlyOwner {
+        require(msg.sender == owner, "you're not owner");
+        _;
     }
 }
 
