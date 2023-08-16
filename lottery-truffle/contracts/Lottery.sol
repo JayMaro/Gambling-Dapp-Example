1a3ffb6ea0d0f67e7911a9c35064273f8be66fe8
KimJinkYU <jinkyu1996@hanmail.net>
2023-08-16 17:07:59 +0900

    Feat: getBalance

--- lottery-truffle/contracts/Lottery.sol
+++ lottery-truffle/contracts/Lottery.sol
@@ -9,8 +9,12 @@ contract Lottery {
         owner = msg.sender;
     }
 
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+
     // 입출금을 위한 payable
-    function enter() payable public {
+    function enter() public payable {
         require(msg.value >= .01 ether, "msg.value should be greater than or equal to 0.01 ether");
         players.push(payable(msg.sender));
     }
