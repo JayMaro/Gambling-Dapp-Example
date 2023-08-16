2c5a7239797d1a996bc88282bb212fe3af7878f9
KimJinkYU <jinkyu1996@hanmail.net>
2023-08-16 17:13:03 +0900

    Feat: getPlayers

--- lottery-truffle/contracts/Lottery.sol
+++ lottery-truffle/contracts/Lottery.sol
@@ -14,6 +14,11 @@ contract Lottery {
         return address(this).balance;
     }
 
+    // 플레이어 목록 반환, player는 storage에 저장된 값이므로 memory로 설정한 뒤 반환
+    function getPlayers() public view returns (address payable[] memory) {
+        return players;
+    }
+
     // 사용자 참가, 입출금을 위한 payable 설정
     function enter() public payable {
         require(msg.value >= .01 ether, "msg.value should be greater than or equal to 0.01 ether");
