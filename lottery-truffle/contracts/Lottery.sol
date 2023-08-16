01e028455ecb70690910573142cdd1f2c3a5b046
KimJinkYU <jinkyu1996@hanmail.net>
2023-08-16 17:10:09 +0900

    Docs: Lottery.sol comment update

--- lottery-truffle/contracts/Lottery.sol
+++ lottery-truffle/contracts/Lottery.sol
@@ -9,11 +9,12 @@ contract Lottery {
         owner = msg.sender;
     }
 
+    // 입금된 금액 반환
     function getBalance() public view returns (uint256) {
         return address(this).balance;
     }
 
-    // 입출금을 위한 payable
+    // 사용자 참가, 입출금을 위한 payable 설정
     function enter() public payable {
         require(msg.value >= .01 ether, "msg.value should be greater than or equal to 0.01 ether");
         players.push(payable(msg.sender));
